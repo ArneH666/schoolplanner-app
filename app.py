e1af80492ce93614213030036359f4259d482361
Arne Hilbig <111295854+ArneH666@users.noreply.github.com>
2024-05-13 22:42:37 +0200

    Upload current state

--- app.py
+++ app.py
@@ -25,5 +25,31 @@ def login():
             return flask.redirect(flask.url_for("home"))
 
 
+@app.route("/students", methods=["GET", "POST"])
+def students():
+    if flask.request.method == "GET":
+        return flask.render_template("students.html")
+    elif flask.request.method == "POST":
+        if "delete" in flask.request.form:
+            # TODO: Delete User Data
+            return flask.render_template("students.html")
+        if "change" in flask.request.form:
+            return app.redirect(flask.url_for("modify_student", student_id=flask.request.form["id"]))
+        if "new" in flask.request.form:
+            return app.redirect(flask.url_for("new"))
+
+
+@app.route("/students/modify/<student_id>")
+def modify_student(student_id: str):
+    if flask.request.method == "GET":
+        return "student " + student_id
+
+
+@app.route("/students/new", methods=["GET", "POST"])
+def new():
+    if flask.request.method == "GET":
+        return "new"
+
+
 if __name__ == "__main__":
     app.run(debug=True)
